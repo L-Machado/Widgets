c02e42929b4fd5a51856a8c31c2dd487ab8feb48
Lucas M Costa <lmdcosta@uniara.edu.br>
2024-03-23 00:39:28 -0300

    First Commi

---
+++ android/app/src/main/kotlin/com/example/exeples/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exeples
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MeuMaterialApp());
+
+class MeuMaterialApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Aplicação test'),
+        ),
+        drawer: const Drawer(),
+        body: const Center(
+          child: Text('Principal'),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Principal'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Configurações')
+          ],
+        ),
+      ),
+    );
+  }
+}
